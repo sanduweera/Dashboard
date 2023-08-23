3378fdd55becd5aa4a7f55420d52f02f3e016059
Kawyanethma Walisundara <kawyanethma@gmail.com>
2023-08-23 20:50:16 +0530

    firebase connected

--- firebaseLogin.js
+++ firebaseLogin.js
@@ -1,43 +1,58 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyC98k--pr_8NqWy6l9sBRsnDlTH8l2-eR8",
-    authDomain: "gpsd-3b169.firebaseapp.com",
-    databaseURL: "https://gpsd-3b169-default-rtdb.firebaseio.com",
-    projectId: "gpsd-3b169",
-    storageBucket: "gpsd-3b169.appspot.com",
-    messagingSenderId: "690462553077",
-    appId: "1:690462553077:web:f7e0e007f9fc1463c266bc"
-  };
-firebase.initializeApp(firebaseConfig);
-var db = firebase.firestore();
+// setting up firebase with our website
+const firebaseApp = firebase.initializeApp({
+  apiKey: "AIzaSyC98k--pr_8NqWy6l9sBRsnDlTH8l2-eR8",
+  authDomain: "gpsd-3b169.firebaseapp.com",
+  databaseURL: "https://gpsd-3b169-default-rtdb.firebaseio.com",
+  projectId: "gpsd-3b169",
+  storageBucket: "gpsd-3b169.appspot.com",
+  messagingSenderId: "690462553077",
+  appId: "1:690462553077:web:f7e0e007f9fc1463c266bc",
+});
+const db = firebaseApp.firestore();
+const auth = firebaseApp.auth();
 
-function ErrorMsg(){
-    var nic = document.getElementById("NIC").value;
-    var name = document.getElementById("name").value;
-    
-    if (name == "" || nic == "") {
-        alert("Empty Fields");
-     }
-     else{
-        console.log("connecting to firebase...");
-        console.log(nic);
-        console.log(name);
-      alert('error');
-//         const docRef = db.collection('admins')
-//    .where("Name", "==", name)
-//    .where("id", "==", nic);
-// // ^ separate .where()
-
-// docRef.get().then((snapshot) => {
-//   if (snapshot.empty) {
-//     // no docs matched
-//     console.log("Invalid Credentials")
-//   } else {
-//     console.log(snapshot.docs[0].data())
-//     .then(function() {
-//       window.location.href = "/Dashboard.html";
-//     });
-//   }
-// })
+// Sign up function
+const signUp = () => {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  console.log(email, password)
+  // firebase code
+  firebase.auth().createUserWithEmailAndPassword(email, password)
+      .then((result) => {
+          // Signed in 
+          document.write("You are Signed Up")
+          console.log(result)
+          // ...
+      })
+      .catch((error) => {
+          console.log(error.code);
+          console.log(error.message)
+          // ..
+      });
 }
 
+// Sign In function
+const signIn = () => {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  // firebase code
+  firebase.auth().signInWithEmailAndPassword(email, password)
+      .then((result) => {
+          // Signed in 
+          alert('Sign in');
+          location.replace("Dashboard.html");
+      })
+      .catch((error) => {
+     
+          alert(error.message);
+
+      });
+
+
+const auth = getAuth();
+signOut(auth).then(() => {
+  // Sign-out successful.
+}).catch((error) => {
+  // An error happened.
+});
 }
